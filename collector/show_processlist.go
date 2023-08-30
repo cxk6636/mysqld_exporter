6c2e5f8280f08886f2b7feac413c9b7f3a00e59b
chenxiaokang <chenxiaokang@beyondcent.com>
2023-08-30 15:04:02 +0800

    show processlist

--- collector/show_processlist.go
+++ collector/show_processlist.go
@@ -9,31 +9,28 @@ import (
 )
 
 const showProcesslistQuery = "SHOW PROCESSLIST"
+const totalName = "total"
+const activeName = "active"
 
-var (
-	showprocesslistDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "cluster", "show_processlist_info"),
-		"xxxxxx",
-		[]string{"type", "category", "sign"}, nil)
-)
+var showProcesslistDesc = prometheus.NewDesc(
+	prometheus.BuildFQName(namespace, "cluster", "show_processlist_info"),
+	" Count connections",
+	[]string{"type", "category", "sign"}, nil)
 
 type ScrapShowProcesslist struct {
 }
 
 func (ScrapShowProcesslist) Name() string {
-	return "showprocesslist"
+	return "show.process.list"
 }
 
 func (ScrapShowProcesslist) Help() string {
-	return "xxxxxxxxxx"
+	return "count connections "
 }
 
 func (ScrapShowProcesslist) Version() float64 {
 	return 5.1
 }
-
-const total_name = "total"
-const active_name = "active"
 
 func (ScrapShowProcesslist) Scrape(ctx context.Context, dbcon *sql.DB, ch chan<- prometheus.Metric, logger log.Logger) error {
 	queryContext, err := dbcon.QueryContext(ctx, showProcesslistQuery)
@@ -47,11 +44,11 @@ func (ScrapShowProcesslist) Scrape(ctx context.Context, dbcon *sql.DB, ch chan<-
 	var Info sql.NullString
 
 	//用户统计
-	userCounts := make(map[string]map[string]uint32)
+	userCounts := make(map[string]map[string]float64)
 	//来源统计
-	hostCounts := make(map[string]map[string]uint32)
+	hostCounts := make(map[string]map[string]float64)
 	//数据库统计
-	dbCounts := make(map[string]map[string]uint32)
+	dbCounts := make(map[string]map[string]float64)
 
 	for queryContext.Next() {
 		var dbt, state sql.NullString
@@ -62,65 +59,60 @@ func (ScrapShowProcesslist) Scrape(ctx context.Context, dbcon *sql.DB, ch chan<-
 		Command = sanitizeState(Command)
 		if Host == "" {
 			Host = "unknown"
+		} else {
+			Host = strings.Split(Host, ":")[0]
 		}
-		Host = strings.Split(Host, ":")[0]
 		db = dbt.String
 
 		//init map
 		if _, ok := userCounts[User]; !ok {
-			userCounts[User] = make(map[string]uint32)
+			userCounts[User] = make(map[string]float64)
 		}
-		if _, ok := userCounts[User][total_name]; !ok {
-			userCounts[User][total_name] = 0
-			userCounts[User][active_name] = 0
+		if _, ok := userCounts[User][totalName]; !ok {
+			userCounts[User][totalName] = 0
+			userCounts[User][activeName] = 0
 		}
 
 		if _, ok := hostCounts[Host]; !ok {
-			hostCounts[Host] = make(map[string]uint32)
+			hostCounts[Host] = make(map[string]float64)
 		}
-		if _, ok := hostCounts[Host][total_name]; !ok {
-			hostCounts[Host][total_name] = 0
-			hostCounts[Host][active_name] = 0
+		if _, ok := hostCounts[Host][totalName]; !ok {
+			hostCounts[Host][totalName] = 0
+			hostCounts[Host][activeName] = 0
 		}
 
 		if _, ok := dbCounts[db]; !ok {
-			dbCounts[db] = make(map[string]uint32)
+			dbCounts[db] = make(map[string]float64)
 		}
-		if _, ok := dbCounts[db][total_name]; !ok {
-			dbCounts[db][total_name] = 0
-			dbCounts[db][active_name] = 0
+		if _, ok := dbCounts[db][totalName]; !ok {
+			dbCounts[db][totalName] = 0
+			dbCounts[db][activeName] = 0
 		}
 
-		userCounts[User][total_name] += 1
-		hostCounts[Host][total_name] += 1
-		dbCounts[db][total_name] += 1
+		userCounts[User][totalName] += 1
+		hostCounts[Host][totalName] += 1
+		dbCounts[db][totalName] += 1
 
 		if Command == "query" {
-			userCounts[User][active_name] += 1
-			hostCounts[Host][active_name] += 1
-			dbCounts[db][active_name] += 1
+			userCounts[User][activeName] += 1
+			hostCounts[Host][activeName] += 1
+			dbCounts[db][activeName] += 1
 		}
 	}
 
 	for key, value := range userCounts {
-		total := float64(value[total_name])
-		active := float64(value[active_name])
-		ch <- prometheus.MustNewConstMetric(showprocesslistDesc, prometheus.GaugeValue, total, "user", key, total_name)
-		ch <- prometheus.MustNewConstMetric(showprocesslistDesc, prometheus.GaugeValue, active, "user", key, active_name)
+		ch <- prometheus.MustNewConstMetric(showProcesslistDesc, prometheus.GaugeValue, value[totalName], "user", key, totalName)
+		ch <- prometheus.MustNewConstMetric(showProcesslistDesc, prometheus.GaugeValue, value[activeName], "user", key, activeName)
 	}
 
 	for key, value := range hostCounts {
-		total := float64(value[total_name])
-		active := float64(value[active_name])
-		ch <- prometheus.MustNewConstMetric(showprocesslistDesc, prometheus.GaugeValue, total, "host", key, total_name)
-		ch <- prometheus.MustNewConstMetric(showprocesslistDesc, prometheus.GaugeValue, active, "host", key, active_name)
+		ch <- prometheus.MustNewConstMetric(showProcesslistDesc, prometheus.GaugeValue, value[totalName], "host", key, totalName)
+		ch <- prometheus.MustNewConstMetric(showProcesslistDesc, prometheus.GaugeValue, value[activeName], "host", key, activeName)
 	}
 
 	for key, value := range dbCounts {
-		total := float64(value[total_name])
-		active := float64(value[active_name])
-		ch <- prometheus.MustNewConstMetric(showprocesslistDesc, prometheus.GaugeValue, total, "db", key, total_name)
-		ch <- prometheus.MustNewConstMetric(showprocesslistDesc, prometheus.GaugeValue, active, "db", key, active_name)
+		ch <- prometheus.MustNewConstMetric(showProcesslistDesc, prometheus.GaugeValue, value[totalName], "db", key, totalName)
+		ch <- prometheus.MustNewConstMetric(showProcesslistDesc, prometheus.GaugeValue, value[activeName], "db", key, activeName)
 	}
 
 	return nil
